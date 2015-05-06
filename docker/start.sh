7583e21fb44169c2ea112b898c52976cac552a6b
m <michal@mdevel.net>
2015-05-06 13:02:49 +0200

    improved docker start script

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+update_file() {
+    local file=$1
+    local var_name=$2
+    local default=$3
+    if [ -z "$default" -a -z "${!var_name}" ]; then
+        echo "$var_name is unset"
+        exit 1
+    fi
+    sed -i "s|$var_name|${!var_name:-$default}|" $file
+}
+
 mkdir -p /volume/log
 mkdir -p /volume/run
 mkdir -p /volume/recovery
@@ -7,31 +18,13 @@ touch /volume/log/zabbix_server.log
 
 chown -R zabbix:zabbix /volume/*
 
-if [ -z "$MYSQL_PORT_3306_TCP_ADDR" ]; then
-    echo "MYSQL_PORT_3306_TCP_ADDR is unset"
-    exit 1
-fi
-sed -i "s/MYSQL_PORT_3306_TCP_ADDR/$MYSQL_PORT_3306_TCP_ADDR/" /opt/zabbix/zabbix_server.conf
+CONF=/opt/zabbix/zabbix_server.conf
 
-
-if [ -z "$MYSQL_ENV_MYSQL_ROOT_PASSWORD" ]; then
-    echo "MYSQL_ENV_MYSQL_ROOT_PASSWORD is unset"
-    exit 1
-fi
-sed -i "s/MYSQL_ENV_MYSQL_ROOT_PASSWORD/$MYSQL_ENV_MYSQL_ROOT_PASSWORD/" /opt/zabbix/zabbix_server.conf
-
-if [ -z "$ROUTER_QUEUE_ADDR" ]; then
-    echo "ROUTER_QUEUE_ADDR is unset"
-    exit 1
-fi
-sed -i "s|ROUTER_QUEUE_ADDR|$ROUTER_QUEUE_ADDR|" /opt/zabbix/zabbix_server.conf
-
-if [ -z "$ERROR_QUEUE_ADDR" ]; then
-    echo "ERROR_QUEUE_ADDR is unset"
-    exit 1
-fi
-sed -i "s|ERROR_QUEUE_ADDR|$ERROR_QUEUE_ADDR|" /opt/zabbix/zabbix_server.conf
+update_file $CONF MYSQL_PORT_3306_TCP_ADDR
+update_file $CONF MYSQL_ENV_MYSQL_ROOT_PASSWORD
+update_file $CONF ROUTER_QUEUE_ADDR
+update_file $CONF ERROR_QUEUE_ADDR
 
 mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" < /opt/zabbix/zabbix_schema.sql
 
-/opt/zabbix/zabbix_server -c /opt/zabbix/zabbix_server.conf && tail -F /volume/log/zabbix_server.log
+/opt/zabbix/zabbix_server -c $CONF && tail -F /volume/log/zabbix_server.log
