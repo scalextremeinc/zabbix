ca1efc051905041efd51c5df600a6dc2d7521ec3
m <michal@mdevel.net>
2015-07-14 16:39:44 +0200

    added alert scripts

---
+++ alertscripts/sxaction.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import logging
+import os
+import sys 
+import httplib, urllib
+
+def sxaction(monitor_host, monitor_user, monitor_pass, action_payload):
+    params = urllib.urlencode({
+        'username': monitor_user,
+        'password': monitor_pass,
+        'data': action_payload
+    })
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "text/plain"}
+    url_path = "/monitor/api/action.php"
+
+    logging.debug('Request POST, url: https://%s%s, headers: %s, body: %s',
+            monitor_host, url_path, headers, params)
+    
+    conn = httplib.HTTPSConnection(monitor_host)
+    conn.request("POST", url_path, params, headers)
+    response = conn.getresponse()
+    body = response.read()
+    logging.debug('Response, code: %s %s, headers: %s, body: %s',
+           response.status, response.reason, response.getheaders(), body)
+
+if __name__ == '__main__':
+    log_dir = os.environ.get('LOG_DIR', '/var/log')
+    log_file = log_dir + '/sxaction.log'
+    logging.basicConfig(filename=log_file, level=logging.DEBUG,
+            format='%(levelname)s - %(asctime)s - %(name)s - %(message)s')
+
+    logging.info('Action script called, cmd line args: %s', sys.argv)
+
+    monitor_host = os.environ.get('MONITORIP', 'MONITORIP_ENV_MISSING')
+    monitor_user = os.environ.get('MONITORUSER', 'MONITORUSER_ENV_MISSING')
+    monitor_pass = os.environ.get('MONITORPASS', 'MONITORPASS_ENV_MISSING')
+    action_payload = sys.argv[3]
+    
+    sxaction(monitor_host, monitor_user, monitor_pass, action_payload)
