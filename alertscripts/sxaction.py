2647e2517234de087ba4e2c3559cd3811bb11e81
m <michal@mdevel.net>
2015-08-13 12:37:10 +0200

    improved action scripts

--- alertscripts/sxaction.py
+++ alertscripts/sxaction.py
@@ -2,40 +2,44 @@
 import logging
 import os
 import sys 
-import httplib, urllib
+import urllib
+import urllib2
 
-def sxaction(monitor_host, monitor_user, monitor_pass, action_payload):
-    logging.info('Action, payload: %.120s ...', action_payload)
-    params = urllib.urlencode({
-        'username': monitor_user,
-        'password': monitor_pass,
+LOG = logging.getLogger(__name__)
+
+REQUEST_HEADERS = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": "text/plain"}
+
+def sxaction(url, user, passwd, action_payload):
+    if not LOG.isEnabledFor(logging.DEBUG):
+        LOG.info('Action, payload: %.120s ...', action_payload)
+
+    data = urllib.urlencode({
+        'username': user,
+        'password': passwd,
         'data': action_payload
     })
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Accept": "text/plain"}
-    url_path = "/monitor/api/action.php"
 
-    logging.debug('Request POST, url: https://%s%s, headers: %s, body: %s',
-            monitor_host, url_path, headers, params)
-    
-    conn = httplib.HTTPSConnection(monitor_host)
-    conn.request("POST", url_path, params, headers)
-    response = conn.getresponse()
-    body = response.read()
-    logging.debug('Response, code: %s %s, headers: %s, body: %s',
-           response.status, response.reason, response.getheaders(), body)
+    LOG.debug('Request, url: %s, data: %s', url, data)
+
+    request = urllib2.Request(url, data, REQUEST_HEADERS)
+    response = urllib2.urlopen(request)
+
+    if LOG.isEnabledFor(logging.DEBUG):
+        LOG.debug('Response, code: %s, data: %s',
+                response.getcode(), response.read())
 
 if __name__ == '__main__':
     log_dir = os.environ.get('LOG_DIR', '/var/log')
     log_file = os.path.join(log_dir, 'sxaction.log')
-    log_level = os.environ.get('LOG_LEVEL', 'INFO').upper()
+    log_level = os.environ.get('ACTION_LOG_LEVEL', 'INFO').upper()
     logging.basicConfig(filename=log_file, level=logging.getLevelName(log_level),
             format='%(levelname)s - %(asctime)s - %(name)s - %(message)s')
 
-    monitor_host = os.environ.get('MONITORIP', 'MONITORIP_ENV_MISSING')
-    monitor_user = os.environ.get('MONITORUSER', 'MONITORUSER_ENV_MISSING')
-    monitor_pass = os.environ.get('MONITORPASS', 'MONITORPASS_ENV_MISSING')
+    sx_action_url = os.environ.get('SX_ACTION_URL', 'SX_ACTION_URL_ENV_MISSING')
+    sx_user = os.environ.get('SX_USER', 'SX_USER_ENV_MISSING')
+    sx_pass = os.environ.get('SX_PASS', 'SX_PASS_ENV_MISSING')
     action_payload = sys.argv[3]
     
-    sxaction(monitor_host, monitor_user, monitor_pass, action_payload)
+    sxaction(sx_action_url, sx_user, sx_pass, action_payload)
